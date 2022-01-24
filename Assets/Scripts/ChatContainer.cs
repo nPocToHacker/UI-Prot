1bc4151b37a241d04564eae6bb17336b2287cf37
nPocToHacker <98032132+nPocToHacker@users.noreply.github.com>
2022-01-24 11:53:33 +0300

    Add files via upload

--- Assets/Scripts/ChatContainer.cs
+++ Assets/Scripts/ChatContainer.cs
@@ -7,6 +7,10 @@ public class ChatContainer : MonoBehaviour
 {
     public GameObject messageTemplate, chatWindow;
 
+    public TextMeshProUGUI getNameFrom;
+
+    private string playerName;
+
     public int messageCapacity = 10;
 
     [SerializeField]
@@ -14,29 +18,36 @@ public class ChatContainer : MonoBehaviour
 
     void Start()
     {
+        playerName = "Player";
+    }
 
+    public void updatePlayerName()
+    {
+        playerName = getNameFrom.text;
     }
 
     public void AddMessage(TextMeshProUGUI inputField)
     {
         string text = inputField.text;
+
         if (messageContainer.Count >= messageCapacity)
         {
             Destroy(messageContainer[0].textObject.gameObject);
             messageContainer.Remove(messageContainer[0]);
         }
 
-        Message input = new Message();
+        Message newMessage = new Message();
 
-        input.text = text;
+        newMessage.senderName = playerName;
+        newMessage.text = text;
 
         GameObject newText = Instantiate(messageTemplate, chatWindow.transform);
 
-        input.textObject = newText.GetComponent<TextMeshProUGUI>();
+        newMessage.textObject = newText.GetComponent<TextMeshProUGUI>();
 
-        input.textObject.SetText(input.text);
+        newMessage.textObject.SetText(newMessage.senderName + ": " + newMessage.text);
 
-        messageContainer.Add(input);
+        messageContainer.Add(newMessage);
     }
 }
 
@@ -45,4 +56,5 @@ public class Message
 {
     public string text;
     public TextMeshProUGUI textObject;
+    public string senderName;
 }
